deb5c0e8fe51dd3bbb5c3bf1eaafe52d22d3db69
Tinh <tinhkien@gmail.com>
2024-11-22 14:39:04 +0700

    tách css featured product ra nhỏ dành riêng cho css product

--- themes/giaiphapweb/gpw-templates/woocommerce/product.php
+++ themes/giaiphapweb/gpw-templates/woocommerce/product.php
@@ -4,16 +4,16 @@
  * * Template for products featured.
  */
 ?>
-<div class="<?= $args['class'] ?>">
-  <a class="item-product__image" href="<?= esc_url($args['product-link']); ?>">
+<div class="product-detail <?= $args['class'] ?>">
+  <a class="product-detail__image" href="<?= esc_url($args['product-link']); ?>">
     <?= $args['product-image']; ?>
   </a>
   <a href="<?= esc_url($args['product-link']); ?>">
-    <p class="item-product__title">
+    <p class="product-detail__title">
       <?= esc_html($args['product-title']) ?>
     </p>
   </a>
-  <p class="item-product__price">
+  <p class="product-detail__price">
     <?= $args['product-price']; ?>
   </p>
   <?php get_template_part(
