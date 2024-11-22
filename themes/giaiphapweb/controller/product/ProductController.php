afd4b08b7accc2134afb6a8a2391bbf8e3368a8f
Tinh <tinhkien@gmail.com>
2024-11-22 10:50:10 +0700

    feature featured produtcs

--- themes/giaiphapweb/controller/product/ProductController.php
+++ themes/giaiphapweb/controller/product/ProductController.php
@@ -7,7 +7,7 @@ class ProductController
     {
         $args = array(
             'post_type' => 'product',
-            'posts_per_page' => -1,
+            'posts_per_page' => 4,
             'tax_query' => array(
                 array(
                     'taxonomy' => 'product_visibility',
@@ -25,17 +25,17 @@ class ProductController
         if ($query->have_posts()) {
             while ($query->have_posts()) {
                 $query->the_post();
-                $product_id = get_the_ID();
-                $product = wc_get_product($product_id);
-                $product_image = get_the_post_thumbnail($product_id, 'medium');
-                $product_price = $product->get_price_html();
+                $productId = get_the_ID();
+                $product = wc_get_product($productId);
+                $productImage = get_the_post_thumbnail($productId, 'medium');
+                $price = $product ? $product->get_price_html() : '';
 
                 $products_data[] = array(
-                    'id' => $product_id,
+                    'id' => $productId,
                     'title' => get_the_title(),
                     'link' => get_permalink(),
-                    'image' => $product_image,
-                    'price' => $product_price,
+                    'image' => $productImage,
+                    'price' => $price,
                 );
             }
         }
