487f2bd1a98642d6f6114a0d3dc0fd71252b966e
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2024-11-23 09:13:08 +0700

    added zoom on hover effect

--- themes/giaiphapweb/src/js/components/single-product-gallery.js
+++ themes/giaiphapweb/src/js/components/single-product-gallery.js
@@ -10,11 +10,11 @@ export default class SingleProductGallery {
 
   /**
    * Initializes the Swiper galleries for the product page.
-   * 
+   *
    * This method sets up two Swiper instances:
    * 1. `_thumbGallery`: A thumbnail gallery with specific configurations.
    * 2. `_mainGallery`: The main gallery that uses the thumbnail gallery for navigation.
-   * 
+   *
    * The `_thumbGallery`:
    * The `_mainGallery` is configured with:
    * - `thumbs`: Uses the `_thumbGallery` instance for thumbnail navigation.
@@ -28,7 +28,7 @@ export default class SingleProductGallery {
       breakpoints: {
         550: {
           slidesPerView: 4,
-        }
+        },
       },
     });
     const mainSwiper = new Swiper(this._mainGalleryEl, {
@@ -39,22 +39,25 @@ export default class SingleProductGallery {
     });
   }
   imageZoomOnHover() {
-    const imageContainers = this._mainGalleryEl.querySelectorAll('.swiper-slide');
-    imageContainers.forEach(imageContainer => {
-      const img = imageContainer.querySelector('img');
-      imageContainer.addEventListener('mouseenter', onImageMouseEnter);
-    });
-    const onImageMouseEnter = e => {
+    const onZoom = e => {
       const _self = e.currentTarget;
-      const img = _self.querySelector('img');
+      const img = _self.querySelector('.swiper-slide-active img');
       const x = e.clientX - _self.offsetLeft;
       const y = e.clientY - _self.offsetTop;
       img.style.setProperty('--x', `${x}px`);
       img.style.setProperty('--y', `${y}px`);
       img.style.setProperty('--scale', '2');
     };
-    const onImageMOuseLeave = e => {
+    const offZoom = e => {
+      const _self = e.currentTarget;
+      const img = _self.querySelector('.swiper-slide-active img');
+      img.style.setProperty('--x', 'center');
+      img.style.setProperty('--y', 'center');
+      img.style.setProperty('--scale', '1');
+    };
 
-    }
+    this._mainGalleryEl.addEventListener('mouseenter', onZoom);
+    this._mainGalleryEl.addEventListener('mousemove', onZoom);
+    this._mainGalleryEl.addEventListener('mouseleave', offZoom);
   }
 }
