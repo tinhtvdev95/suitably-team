ad2aa19d32a177938f0b99f64b233b20c6e67c0e
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2024-12-07 23:13:39 +0700

    processing project

--- themes/giaiphapweb/src/js/components/single-product-step-options-selection.js
+++ themes/giaiphapweb/src/js/components/single-product-step-options-selection.js
@@ -1,14 +1,34 @@
 export default class StepsAndOptionSelection {
-  constructor(){
+  constructor(customizeFormData) {
     this.stepsNavItemsSelector = 'customize-popup__nav-item';
     this.navItemActiveClass = `${this.stepsNavItemsSelector}--active`;
     this.navItemDisabledClass = `${this.stepsNavItemsSelector}--disabled`;
 
     this.stepsContentSelector = 'customize-popup__step';
     this.stepContentActiveClass = `${this.stepsContentSelector}--active`;
-    
+
     this.stepOptionsSelector = 'step-option';
     this.stepContentContinueBtnSelector = 'customize-popup__step-continue-btn';
+
+    this.productDetailsCustomOptionSelector = 'product-details-custom-option__container';
+    this.productDetailsCustomOptionActiveClass = `${this.productDetailsCustomOptionSelector}--active`;
+    this.productDetailsCustomOptionShowChildrenClass = `${this.productDetailsCustomOptionSelector}--show-children`;
+
+    this.productDetailsCustomOptionTitleSelector = 'product-details-custom-option__title';
+
+    this.customizeFormData = new Proxy(customizeFormData, {
+      set: (target, key, value) => {
+        const isKeyExist = Object.prototype.hasOwnProperty.call(target, key);
+        if (key === 'choose-options' && isKeyExist) {
+          document.querySelectorAll(`.${this.productDetailsCustomOptionActiveClass}`).forEach(container => {
+            container.classList.remove(this.productDetailsCustomOptionActiveClass);
+          });
+        }
+        target[key] = value;
+        return true;
+        
+      }
+    });
 
     this.init();
     this.addEventListeners();
@@ -18,19 +38,21 @@ export default class StepsAndOptionSelection {
     this.stepsNavItems = document.querySelectorAll(`.${this.stepsNavItemsSelector}`);
     this.stepsContent = document.querySelectorAll(`.${this.stepsContentSelector}`);
     this.stepOptions = document.querySelectorAll(`.${this.stepOptionsSelector}`);
+    this.productDetailsCustomOptions = document.querySelectorAll(`.${this.productDetailsCustomOptionSelector}`);
+    this.productDetailsCustomTitles = document.querySelectorAll(`.${this.productDetailsCustomOptionTitleSelector}`);
   }
 
-  addEventListeners(){
+  addEventListeners() {
     this.stepsNavItems.forEach(stepNavItem => {
       const navButton = stepNavItem.querySelector('a');
       navButton.addEventListener('click', event => {
         this.handleSwitchStepWithNavItems(event, stepNavItem, navButton);
       });
-    });   
+    });
 
     this.stepsContent.forEach(stepContent => {
       const continueBtn = stepContent.querySelector(`.${this.stepContentContinueBtnSelector}`);
-      continueBtn.addEventListener('click', event => {
+      continueBtn?.addEventListener('click', event => {
         this.handleContinueStep(event, stepContent);
       });
     });
@@ -40,10 +62,16 @@ export default class StepsAndOptionSelection {
         this.handleSelectingOption(event, stepOption);
       });
     });
+
+    this.productDetailsCustomTitles.forEach(title => {
+      title.addEventListener('click', event => {
+        this.handleProductDetailsCustomOptionTitleClick(event, title);
+      });
+    });
   }
 
   handleSelectingOption(event, stepOption) {
-    if(event.target.tagName === "INPUT") {
+    if (event.target.tagName === 'INPUT') {
       event.stopPropagation();
       return;
     }
@@ -51,48 +79,114 @@ export default class StepsAndOptionSelection {
     const inputName = input.getAttribute('name');
     this.stepOptions.forEach(option => {
       const optionInput = option.querySelector('input');
-      if(optionInput.name === inputName && optionInput.hasAttribute('checked')) {
+      if (optionInput.name === inputName && optionInput.hasAttribute('checked')) {
         optionInput.removeAttribute('checked');
       }
     });
     input.setAttribute('checked', '1');
-  };
+  }
 
   handleContinueStep(event, stepContent) {
     event.preventDefault();
+    const NEXT_STEP_INDEX = 1;
 
-    const nextStepIndex = Array.from(this.stepsContent).indexOf(stepContent) + 1;
+    const stepOptions = stepContent.querySelectorAll(`.${this.stepOptionsSelector} input`);
+  
+    if (!this.validateStepOptions(stepOptions)) return;
+
+    stepOptions.forEach(option => {
+      if (option.hasAttribute('checked')) {
+        this.addDataToFormData(option.getAttribute('name'), option.value, option.dataset.slug);
+      }
+    });
+
+    // console.log(this.customizeFormData);
+
+    const nextStepIndex = Array.from(this.stepsContent).indexOf(stepContent) + NEXT_STEP_INDEX;
     const nextStepNavItem = this.stepsNavItems[nextStepIndex];
     const nextStepNavItemButton = nextStepNavItem.querySelector('a');
+    const nextStepContent = this.stepsContent[nextStepIndex];
+    const stepName = nextStepContent.getAttribute('id');
+
+    if(stepName === 'choose-details') {
+      console.log(this.customizeFormData['choose-options']);
+      switch(this.customizeFormData['choose-options'].slug) {
+        case 'custom-suits':
+          this.setActiveForProductDetailsCustomOption(['custom-jacket', 'pants', 'waistcoat']);
+          break;    
+        case 'custom-jackets':
+          this.setActiveForProductDetailsCustomOption(['custom-jacket']);
+          break;
+        case 'custom-waistcoats':
+          this.setActiveForProductDetailsCustomOption(['waistcoat']);
+          break;
+        case 'custom-pants':
+          this.setActiveForProductDetailsCustomOption(['pants']);
+          break;
+        case 'custom-coats':
+          this.setActiveForProductDetailsCustomOption(['coat']);
+          break;
+        case 'custom-shirts':
+          this.setActiveForProductDetailsCustomOption(['shirt']);
+          break;
+        case 'tuxedo-collection':
+          this.setActiveForProductDetailsCustomOption(['custom-jacket', 'pants', 'waistcoat']);
+          break;
+      }
+    }
 
     this.switchActiveStep(nextStepNavItem, nextStepNavItemButton, true);
   }
 
-  validateStepOptions(stepContent) {
-    const stepOptions = stepContent.querySelectorAll(`.${this.stepOptionsSelector} input`);
-    let isValid = true;
-    stepOptions.forEach(option => {
-      if(!option.hasAttribute('checked')) {
-        isValid = false;
+  setActiveForProductDetailsCustomOption(keys){
+    this.productDetailsCustomOptions.forEach(container => {
+      for(const key of keys) {
+        if(container.getAttribute('id').includes(key)) {
+          container.classList.add(this.productDetailsCustomOptionActiveClass);
+        }
       }
-    });
+    })
+  }
+
+  validateStepOptions(stepOptions) {
+    let isValid = false;
+    let lastInputName = '';
+    for (const option of stepOptions) {
+      const currentInputName = option.getAttribute('name');
+      if (lastInputName !== option.getAttribute('name')) {
+        if (isValid) {
+          break;
+        }
+        lastInputName = option.getAttribute('name');
+      }
+      if (currentInputName === lastInputName && option.hasAttribute('checked')) {
+        isValid = true;
+      }
+    }
     return isValid;
+  }
+
+  addDataToFormData(key, value, slug) {
+    this.customizeFormData[key] = {
+      value,
+      slug,
+    };
   }
 
   handleSwitchStepWithNavItems(event, parent, _self) {
     const isNavItemActive = parent.classList.contains(this.navItemActiveClass);
     const isNavItemDisabled = parent.classList.contains(this.navItemDisabledClass);
 
-    if(isNavItemActive || isNavItemDisabled) return;
+    if (isNavItemActive || isNavItemDisabled) return;
 
     this.switchActiveStep(parent, _self);
   }
-  
+
   switchActiveStep(navItem, navItemButton, isContinueClick = false) {
     const oldActiveNavItem = document.querySelector(`.${this.navItemActiveClass}`);
     oldActiveNavItem.classList.remove(this.navItemActiveClass);
-    
-    if(isContinueClick) {
+
+    if (isContinueClick) {
       navItem.classList.remove(this.navItemDisabledClass);
       oldActiveNavItem.classList.add(`${this.stepsNavItemsSelector}--completed`);
     }
@@ -105,4 +199,10 @@ export default class StepsAndOptionSelection {
     document.querySelector(`.${this.stepContentActiveClass}`).classList.remove(this.stepContentActiveClass);
     targetStep.classList.add(this.stepContentActiveClass);
   }
+
+  handleProductDetailsCustomOptionTitleClick(event, _self) {
+    const parent = _self.parentElement;
+    parent.classList.toggle(this.productDetailsCustomOptionShowChildrenClass);
+  }
 }
+
--- themes/giaiphapweb/src/js/export/gpw-single-product.js
+++ themes/giaiphapweb/src/js/export/gpw-single-product.js
@@ -1,14 +1,13 @@
 import SingleProductGallery from "../components/single-product-gallery";
-import SliderProductOptionFields from "../components/slider-product-option-fields";
 import SingleProductCustomizeOption from "../components/single-product-customize-option"
 import CustomizePopupController from "../components/single-product-customize-popup";
 import SingleProductStepOptionsSelection from "../components/single-product-step-options-selection";
 document.addEventListener('DOMContentLoaded', function () {
+  const customizeFormData = {};
+
   // * Galleries swiper
   const singleProductGallery = new SingleProductGallery();
-  // const sliderOptions = new SliderProductOptionFields();
   const sliderSingleProductCustomizeOption = new SingleProductCustomizeOption();
-  const sliderOptions = new SliderProductOptionFields();
   const customizePopup = new CustomizePopupController();
-  const stepsAndOptionSelection = new SingleProductStepOptionsSelection();
+  const stepsAndOptionSelection = new SingleProductStepOptionsSelection(customizeFormData);
 });
